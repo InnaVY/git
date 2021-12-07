d8febfc9a84bc1a52b6d1adebcb9d74ead27da10
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-12-07 15:55:16 +0200

    2 helloword added

---
+++ src/com/pk/io/HelloWord1.java
@@ -0,0 +1,7 @@
+package com.pk.io;
+
+public class HelloWord1 {
+    public static void main(String [] args){
+        System.out.println("Hello git");
+    }
+}
---
+++ src/com/pk/io/HelloWord2.java
@@ -0,0 +1,7 @@
+package com.pk.io;
+
+public class HelloWord2 {
+    public static void main(String [] args){
+        System.out.println("git2");
+    }
+}
