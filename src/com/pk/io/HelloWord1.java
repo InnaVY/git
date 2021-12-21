6b89d8e48535c73b6c83fded7a241c48170b002a
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-12-21 10:15:29 +0200

    HelloWord1 added new line

--- src/com/pk/io/HelloWord1.java
+++ src/com/pk/io/HelloWord1.java
@@ -4,5 +4,6 @@ public class HelloWord1 {
     public static void main(String [] args){
         System.out.println("Hello git");
         System.out.println("Hi");
+        System.out.println("Hi again");
     }
 }
